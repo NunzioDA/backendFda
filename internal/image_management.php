1447d22bd83d1d557eae42f289f5bf5c545cc6f5
NunzioDA <nunziodamore@gmail.com>
2023-12-21 14:45:36 +0100

    Conversione png ad 8 bit

--- internal/image_management.php
+++ internal/image_management.php
@@ -1,5 +1,23 @@
 <?php
 	
+	function convertPNGto8bitPNGAndSave ($srcimage, $destPath) {
+		$srcimage = imageCreateFromString($srcimage);
+		$width  = imagesx($srcimage);
+		$height = imagesy($srcimage);
+
+		$img = imagecreatetruecolor($width, $height);
+		$bga = imagecolorallocatealpha($img, 0, 0, 0, 127);
+		imagecolortransparent($img, $bga);
+		imagefill($img, 0, 0, $bga);
+		imagecopy($img, $srcimage, 0, 0, 0, 0, $width, $height);
+		imagetruecolortopalette($img, false, 255);
+		imagesavealpha($img, true);
+
+		imagepng($img, $destPath);
+		imagedestroy($img);
+
+	 }
+	
 	function getImagesDirectory()
 	{
 		$imagesPath = '../images';
@@ -15,7 +33,6 @@
 	
 	function saveBase64Image($base64_encoded_string)
 	{
-		global $imagesPath;
 		$name = bin2hex(openssl_random_pseudo_bytes(16));	
 		$files = scandir(getImagesDirectory());
 		
@@ -31,9 +48,8 @@
 			}
 		}while(!$check);
 		
-		$decodedImage = base64_decode($base64_encoded_string); 
-		
-		file_put_contents(getImagePath().$name, $decodedImage);
+		$decodedImage = base64_decode($base64_encoded_string); 			
+		convertPNGto8bitPNGAndSave($decodedImage, getImagePath().$name);
 		
 		return $name;
 	}

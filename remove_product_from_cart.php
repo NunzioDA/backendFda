7d720698e07fd3347e771c2bafe1d6d6219d525c
NunzioDA <nunziodamore@gmail.com>
2023-12-25 13:35:31 +0100

    Aggiunta possibilità di rimuovere prodotto totalmente dal carrello

--- remove_product_from_cart.php
+++ remove_product_from_cart.php
@@ -9,6 +9,15 @@
 	$token = _GET_("token");
 	$product_id = _GET_("product_id");
 	
+	
+	if(array_key_exists('remove_all', $_GET))
+	{
+		$remove_all = $_GET["remove_all"];
+	}
+	else{
+		$remove_all = 0;
+	}
+	
 	$cred_manager = new CredentialManager();
 	
 	$db = new DBHandler();
@@ -17,7 +26,13 @@
 	
 	if($cred_manager -> matching_username_token($username, $token))
 	{		
-		$remove_product = "DELETE FROM cart WHERE username=:username AND product_id=:product_id LIMIT 1";
+		$remove_product = "DELETE FROM cart WHERE username=:username AND product_id=:product_id";
+		
+		if($remove_all != "1")
+		{
+			$remove_product .= " LIMIT 1";
+		}		
+		
 		$stmt = $conn -> prepare($remove_product);
 		$stmt -> bindParam(':username', $username);
 		$stmt -> bindParam(':product_id', $product_id);
